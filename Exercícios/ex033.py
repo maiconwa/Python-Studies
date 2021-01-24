5ddc0bdbb278c483ce50e77acb432e4f2ef333f9
Maicon William <73193175+maiconwa@users.noreply.github.com>
2021-01-24 12:38:38 -0300

    Update ex033

--- Exercícios/ex033.py
+++ Exercícios/ex033.py
@@ -1,5 +1,17 @@
-n1 = float(input('Digite o primeiro número: '))
-n2 = float(input('Digite o segundo número: '))
-n3 = float(input('Digite o terceiro número: '))
-if n1 > n2 or n3:
-    
+a = int(input('Digite o primeiro número: '))
+b = int(input('Digite o segundo número: '))
+c = int(input('Digite o terceiro número: '))
+# Verificando quem é o menor
+menor = a
+if b < a and b < c:
+    menor = b
+if c < a and c < b:
+    menor = c
+print('O menor valor digitado foi {}'.format(menor))
+# Verificando quem é o maior
+maior = a
+if b > a and c > a:
+    maior = b
+if c > a and c > b:
+    maior = c
+print('O maior valor digitado foi {}'.format(maior))
