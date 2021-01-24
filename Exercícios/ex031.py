6dc1c0a02c7342a1d030c23cd8bcb15871b3eb7f
Maicon William <73193175+maiconwa@users.noreply.github.com>
2021-01-24 11:56:13 -0300

    Update ex031

--- Exercícios/ex031.py
+++ Exercícios/ex031.py
@@ -1,5 +1,7 @@
 dis = float(input('Qual é a distância da viagem? '))
+print('Você está prestes a começar uma viagem de {}Km'.format(dis))
 if dis <= 200:
-    print('O valor total da passagem é de R${}'.format(dis * .5))
+    print('O valor total da passagem é de R${:.2f}'.format(dis * .5))
 else:
-    print('O valor total da passagem é de R${}'.format(dis * .45))
+    print('O valor total da passagem é de R${:.2f}'.format(dis * .45))
+print('Boa viagem!')
