611323e910ec8385476c978e07ff8de0fb146677
Maicon William <73193175+maiconwa@users.noreply.github.com>
2021-01-25 10:00:58 -0300

    Update ex039

--- Exercícios/ex039.py
+++ Exercícios/ex039.py
@@ -10,9 +10,10 @@ nascimento = int(input('Digite o ano em que você nasceu: '))
 ano = int(date.today().year)
 
 if ano - nascimento > int(18):
-    print('Você tem {} anos, seu periodo de alistamento já passou.'.format(ano - nascimento))
+    alistamento = (ano - nascimento) - 18
+    print('Você tem {} anos, seu periodo de alistamento já passou faz {} anos.'.format(ano - nascimento, alistamento))
 elif ano - nascimento < int(18):
-    restante = 18 - (ano - nascimento)
-    print('Você tem {} anos, lembre-se que você ainda irá se alistar em {} anos.'.format(ano - nascimento, restante))
+    alistamento = 18 - (ano - nascimento)
+    print('Você tem {} anos, lembre-se que você ainda irá se alistar em {} anos.'.format(ano - nascimento, alistamento))
 elif ano - nascimento == int(18):
     print('Você tem {} anos. Chegou a hora de se alistar.'.format(ano - nascimento))
