c7035e2ead3369fb73fd710773cd47ebbb7bc437
Maicon William <73193175+maiconwa@users.noreply.github.com>
2021-01-25 16:03:20 -0300

    Update ex043

--- Exercícios/ex043.py
+++ Exercícios/ex043.py
@@ -7,16 +7,16 @@ com a tabela abaixo:
 - Entre 30 e 40: Obesidade
 - Acima de 40: Obesidade mórbida"""
 
-peso = float(input('Digite o seu peso: '))
-altura = float(input('Digite a sua altura: '))
+peso = float(input('Digite o seu peso: (Kg) '))
+altura = float(input('Digite a sua altura: (m) '))
 imc = (peso / (altura ** 2)) * 10000
 if imc < 18.5:
-    print('O seu peso é {}Kg, sua altura é {}m, o IMC é {:.2f}, abaixo do peso.'.format(peso, altura, imc))
+    print('O seu peso é {}Kg, sua altura é {}m, o IMC é {:.1f}, abaixo do peso.'.format(peso, altura, imc))
 elif 18.5 < imc < 25:
-    print('O seu peso é {}Kg, sua altura é {}m, o IMC é {:.2f}, peso ideal.'.format(peso, altura, imc))
+    print('O seu peso é {}Kg, sua altura é {}m, o IMC é {:.1f}, peso ideal.'.format(peso, altura, imc))
 elif 25 < imc < 30:
-    print('O seu peso é {}Kg, sua altura é {}m, o IMC é {:.2f}, sobrepeso.'.format(peso, altura, imc))
+    print('O seu peso é {}Kg, sua altura é {}m, o IMC é {:.1f}, sobrepeso.'.format(peso, altura, imc))
 elif 30 < imc < 40:
-    print('O seu peso é {}Kg, sua altura é {}m, o IMC é {:.2f}, obesidade.'.format(peso, altura, imc))
+    print('O seu peso é {}Kg, sua altura é {}m, o IMC é {:.1f}, obesidade.'.format(peso, altura, imc))
 elif 40 < imc:
-    print('O seu peso é {}Kg, sua altura é {}m, o IMC é {:.2f}, obsidade mórbida. CUIDADO!'.format(peso, altura, imc))
+    print('O seu peso é {}Kg, sua altura é {}m, o IMC é {:.1f}, obsidade mórbida. CUIDADO!'.format(peso, altura, imc))
