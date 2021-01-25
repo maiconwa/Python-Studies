64a46340d5fd3628582573afa41fcf75555312d1
Maicon William <73193175+maiconwa@users.noreply.github.com>
2021-01-25 09:18:29 -0300

    ex037

--- Exercícios/ex037.py
+++ Exercícios/ex037.py
@@ -3,9 +3,13 @@ Escreva um programa que leia um número inteiro qualquer e peça para o usuário
 
 numero = int(input('Digite um numero inteiro a ser convertido: '))
 conversao = int(input("Digite 1 para binário, 2 para octal e 3 para hexadecimal: "))
+
 if conversao == 1:
-    print('A conversão do numero {} para binário é {}'.format(numero, conversao))
+    convertido = bin(numero)
+    print('A conversão do numero {} para binário é {}'.format(numero, convertido))
 elif conversao == 2:
-    print('A conversão do número {} para octal é {}'.format(numero, conversao))
+    convertido = oct(numero)
+    print('A conversão do número {} para octal é {}'.format(numero, convertido))
 elif conversao == 3:
-    print('A conversão do número {} para hexadecimal é {}'.format(numero, conversao))
+    convertido = hex(numero)
+    print('A conversão do número {} para hexadecimal é {}'.format(numero, convertido))
