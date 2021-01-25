9b0b961267021ade2a46dbc2553bfc71872d4dfa
Maicon William <73193175+maiconwa@users.noreply.github.com>
2021-01-25 09:52:10 -0300

    ex039

---
+++ Exercícios/ex039.py
@@ -0,0 +1,17 @@
+"""Desafio 39:
+Faça um programa que leia o ano de nascimento de um jovem e informe, de acordo com sua idade:
+- Se ele ainda vai se alistar n serviço militar.
+- Se é a hora de se alistar.
+- Se já passou do tempo do alistamento.
+Seu programa também deverá mostrar o tempo que falta ou que passou do prazo."""
+
+from datetime import date
+nascimento = int(input('Digite o ano em que você nasceu: '))
+ano = int(date.today().year)
+
+if ano - nascimento > int(18):
+    print('Você tem {} anos, seu periodo de alistamento já passou.'.format(ano - nascimento))
+elif ano - nascimento < int(18):
+    print('Você tem {} anos, lembre-se que você ainda irá se alistar.'.format(ano - nascimento))
+elif ano - nascimento == int(18):
+    print('Você tem {} anos. Chegou a hora de se alistar.'.format(ano - nascimento))
