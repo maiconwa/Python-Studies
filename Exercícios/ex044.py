72053686184e40d44b55c422a4be5b945a2af7a2
Maicon William <73193175+maiconwa@users.noreply.github.com>
2021-01-25 16:44:21 -0300

    Update ex044

--- Exercícios/ex044.py
+++ Exercícios/ex044.py
@@ -5,26 +5,38 @@ de pagamento:
 - À vista no cartão: 5% de desconto
 - Em até 2x no cartão: preço normal
 - Em 3x ou mais no cartão: 20% de juros"""
-
-produto = float(input('Qual é o valor do produto? '))
-print('-=-' * 20)
+print('-=-' * 14)
+print('================== LOJA ==================')
+print('-=-' * 14)
+produto = float(input('Qual é o valor do produto? R$ '))
+print('-=-' * 14)
 print('Digite a forma de pagamento conforme abaixo: ')
-print('-=-' * 20)
-print('Digite 1, para à vista dinheiro/cheque')
-print('Digite 2, para à vista no cartão')
-print('Digite 3, para parcelamento em até 2x no cartão')
-print('Digite 4, para parcelamento em mais de 3x no cartão')
-print('-=-' * 20)
-pagamento = float(input('Digite a opção desejada conforme descrito acima: '))
+print('-=-' * 14)
+print('''[1] para à vista dinheiro/cheque
+[2] para à vista no cartão'
+[3] para parcelamento em até 2x no cartão
+[4] para parcelamento em mais de 3x no cartão''')
+print('-=-' * 14)
+pagamento = int(input('Digite a opção desejada conforme descrito acima: '))
 if pagamento == 1:
-    valorf = (produto * .10) + produto
-    print('O valor do produto é {:.2f}, com desconto de 10% o valor a ser pago é {:.2f}'.format(produto, valorf))
+    desconto = (produto * 10) / 100
+    valorf = produto - desconto
+    print('O valor do produto é R$ {:.2f}, com desconto de 10% o valor a ser pago é R$ {:.2f}'.format(produto, valorf))
+    print('O valor do desconto foi de R$ {:.2f}'.format(desconto))
 elif pagamento == 2:
-    valorf = (produto * .05) + produto
-    print('O valor do produto é {:.2f}, com desconto de 05% o valor a ser pago é {:.2f}'.format(produto, valorf))
+    desconto = (produto * 5) / 100
+    valorf = produto - desconto
+    print('O valor do produto é R$ {:.2f}, com desconto de 05% o valor a ser pago é R$ {:.2f}'.format(produto, valorf))
+    print('O valor do desconto foi de R$ {:.2f}'.format(desconto))
 elif pagamento == 3:
-    print('O valor a ser pago no parcelamento em 2 vezes é {:.2f}'.format(produto))
+    print('O valor a ser pago no parcelamento em 2 vezes é R$ {:.2f}'.format(produto))
 elif pagamento == 4:
     parcelamento = float(input('Em quantas vezes o produto será parcelado? '))
-    valorf = ((produto * .20) * parcelamento) + produto
-    print('O valor a ser pago no parcelamento de {} vezes no cartão é de {}'.format(parcelamento, valorf))
+    juros = (produto * 30) / 100
+    valorf = juros + produto
+    print('O parcelamento foi de {:.0f} vezes no cartão. O juros é R$ {:.2f}'.format(parcelamento, juros))
+    print('O valor total a ser pago é R$ {:.2f}'.format(valorf))
+else:
+    print('-=-' * 14)
+    print('Digite uma das oções de [1] a [4].')
+print('===FIM===' * 5)
