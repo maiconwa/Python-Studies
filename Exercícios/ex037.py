8286b9cf55c6dfbbb9be57575b7521fa50c9f63b
Maicon William <73193175+maiconwa@users.noreply.github.com>
2021-01-25 12:50:38 -0300

    Update ex037

--- Exercícios/ex037.py
+++ Exercícios/ex037.py
@@ -2,14 +2,16 @@
 Escreva um programa que leia um número inteiro qualquer e peça para o usuário escolher qual será a base de conversão"""
 
 numero = int(input('Digite um numero inteiro a ser convertido: '))
-conversao = int(input("Digite 1 para binário, 2 para octal e 3 para hexadecimal: "))
-
+print('''Escolha uma das opções abaixo: 
+print('[1] Converter para BINÁRIO.
+print('[2] Converter para OCTAL.
+print('[3] Converter para HEXADECIMAL.''')
+conversao = int(input("Digite a sua opção: "))
 if conversao == 1:
-    convertido = bin(numero)
-    print('A conversão do numero {} para binário é {}'.format(numero, convertido))
+    print('A conversão do numero {} para binário é {}'.format(numero, bin(numero)[2:]))
 elif conversao == 2:
-    convertido = oct(numero)
-    print('A conversão do número {} para octal é {}'.format(numero, convertido))
+    print('A conversão do número {} para octal é {}'.format(numero, oct(numero)[2:]))
 elif conversao == 3:
-    convertido = hex(numero)
-    print('A conversão do número {} para hexadecimal é {}'.format(numero, convertido))
+    print('A conversão do número {} para hexadecimal é {}'.format(numero, hex(numero)[2:]))
+else:
+    print('Opção invalida. Tente novamente.')
