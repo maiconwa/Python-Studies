f37ee5d728d290a9b35efa17fe2d07160b17f65a
Maicon William <73193175+maiconwa@users.noreply.github.com>
2021-01-25 09:28:16 -0300

    ex038

---
+++ Exercícios/ex038.py
@@ -0,0 +1,15 @@
+"""Desafio 038:
+Escreva um programa que leia dois números inteiros e compare-os. Mostrando na tela uma mensagem:
+- O primeiro valor é maior.
+- O segundo valor é maior
+- Não existe valor maior, os dois são iguais"""
+
+num1 = int(input('Digite um o primeiro número inteiro: '))
+num2 = int(input('Digite um o segundo número inteiro: '))
+
+if num1 > num2:
+    print('O primeiro valor, número {} é maior que o número {}.'.format(num1, num2))
+elif num2 > num1:
+    print('O segundo valor, número {} é maior que o número {}.'.format(num2, num1))
+elif num1 == num2:
+    print('Não exite valor maior. O primeiro número {} é igual ao segundo número {}.'.format(num1, num2))
