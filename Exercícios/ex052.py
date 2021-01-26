f41e5cfe1a4652401a3d6fe5871fdee476785105
Maicon William <73193175+maiconwa@users.noreply.github.com>
2021-01-26 15:26:46 -0300

    Update ex052.py

--- Exercícios/ex052.py
+++ Exercícios/ex052.py
@@ -2,11 +2,16 @@
 Faça um programa que leia um número inteiro e diga se ele é ou não um número primo. """
 
 num = int(input('Digite um número: '))
-primo = 0
-for c in range(2, num):
+tot = 0
+for c in range(1, num + 1):
     if num % c == 0:
-        primo += 1
-if primo == 0 and num != 1:
-    print('É primo')
+        print('\033[33m', end='')
+        tot += 1
+    else:
+        print('\033[31m', end='')
+    print('{} '.format(c), end='')
+print('\n\033[mO numero {} foi divisível {} vezes'.format(num, tot))
+if tot == 2:
+    print('E por isso ele é primo!')
 else:
-    print('O número {} tem {} multiplos acima de 2 e abaixo de {}. Portanto não é primo.'.format(num, primo, num))
+    print('E por isso ele não é primo!')
