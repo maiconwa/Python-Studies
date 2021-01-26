e711d377f516d3f956c296c6758110c4110146d4
Maicon William <73193175+maiconwa@users.noreply.github.com>
2021-01-26 11:40:20 -0300

    Update ex052

--- Exercícios/ex051.py
+++ Exercícios/ex051.py
@@ -1,3 +1,10 @@
 """Desafio 51:
-Desenvolva um programa que leia o primeiro termo e a razao de uma PA. No final mostre os 10 orimeiros termos dessa
+Desenvolva um programa que leia o primeiro termo e a razao de uma PA. No final mostre os 10 primeiros termos dessa
 progressão. """
+
+lista = []
+valor = 0
+for c in range(0, 10):
+    valor = str(input('Digite um número de 0 a 9: '))
+    lista += valor
+print(lista)
--- Exercícios/ex052.py
+++ Exercícios/ex052.py
@@ -1,2 +1,12 @@
 """Desafio 052:
 Faça um programa que leia um número inteiro e diga se ele é ou não um número primo. """
+
+num = int(input('Digite um número: '))
+primo = 0
+for c in range(2, num):
+    if num % c == 0:
+        primo += 1
+if primo == 0 and num != 1:
+    print('É primo')
+else:
+    print('O número {} tem {} multiplos acima de 2 e abaixo de {}. Portanto não é primo.'.format(num, primo, num))
