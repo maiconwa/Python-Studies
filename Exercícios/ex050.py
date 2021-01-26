ff43d069e0b0dc9d872eef707db165661282aa70
Maicon William <73193175+maiconwa@users.noreply.github.com>
2021-01-26 14:56:45 -0300

    Update ex050.py

--- Exercícios/ex050.py
+++ Exercícios/ex050.py
@@ -4,10 +4,12 @@ digitado for impar desconsidere-o"""
 
 par = 0
 soma = 0
-for c in range(0, 6):
-    par = int(input('Digite um número inteiro par: '))
+cont = 0
+for c in range(1, 7):
+    par = int(input('Digite o {}º numero inteiro par: '.format(c)))
     if par % 2 == 0:
         soma += par
+        cont += 1
     else:
         print('Valores impares serão desconsiderados.')
-print('A soma dos valores digitados é {}.'.format(soma))
+print('A soma dos valores digitados é {} e você informou {} números pares.'.format(soma, cont))
