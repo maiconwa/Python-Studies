8f8f7b2a612278c45593e8ed8fe83412d2a512ce
Maicon William <73193175+maiconwa@users.noreply.github.com>
2021-01-26 12:20:00 -0300

    Update ex056.py

--- Exercícios/ex056.py
+++ Exercícios/ex056.py
@@ -3,3 +3,4 @@ Desenvolva um programa que leia o nome, idade e sexo de 4 pessoas. No final do p
 - A média de idade do grupo.
 - Qual é o homem mais velho.
 - Quantas mulheres têm menos de 20 anos."""
+
