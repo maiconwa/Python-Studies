2c73cd30b349cc6919fb04df5adb90d184bdb296
Maicon William <73193175+maiconwa@users.noreply.github.com>
2021-01-26 10:19:00 -0300

    Update ex50

--- Exercícios/ex050.py
+++ Exercícios/ex050.py
@@ -2,3 +2,12 @@
 Desenvolva um programa que leia seis números inteiros e mostre a soma apenas daqueles que forem pares. Se o valor
 digitado for impar desconsidere-o"""
 
+par = 0
+soma = 0
+for c in range(0, 6):
+    par = int(input('Digite um número inteiro par: '))
+    if par % 2 == 0:
+        soma += par
+    else:
+        print('Valores impares serão desconsiderados.')
+print('A soma dos valores digitados é {}.'.format(soma))
