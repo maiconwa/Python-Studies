129e327d236d3432f8cc776272dabaca121e95ad
Maicon William <73193175+maiconwa@users.noreply.github.com>
2021-01-26 08:45:43 -0300

    Update ex046

--- Exercícios/ex046.py
+++ Exercícios/ex046.py
@@ -3,12 +3,19 @@ Faça um programa que mostre na tela uma contagem regressiva para o estouro de f
 com uma pausa se 1 segundo entre eles."""
 
 from time import sleep
+import emoji
 print('=-' * 23)
 print('CONTAGEM REGRESSIVA PARA OS FOGOS DE ARTIFICIO')
 print('=-' * 23)
 for c in range(10, 0, -1):
     sleep(1)
     print(c)
+print(emoji.emojize(':fireworks:' * 28))
+print(emoji.emojize(':fireworks:' * 28))
+print(emoji.emojize(':fireworks:' * 28))
 print('=-' * 23)
 print('FELIZ ANO NOVO!!!')
 print('=-' * 23)
+print(emoji.emojize(':fireworks:' * 28))
+print(emoji.emojize(':fireworks:' * 28))
+print(emoji.emojize(':fireworks:' * 28))
