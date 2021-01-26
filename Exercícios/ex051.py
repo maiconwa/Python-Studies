f062655749f7e2a8af24ea59b921122345097839
Maicon William <73193175+maiconwa@users.noreply.github.com>
2021-01-26 15:09:39 -0300

    Update ex051.py

--- Exercícios/ex051.py
+++ Exercícios/ex051.py
@@ -2,9 +2,9 @@
 Desenvolva um programa que leia o primeiro termo e a razao de uma PA. No final mostre os 10 primeiros termos dessa
 progressão. """
 
-lista = []
-valor = 0
-for c in range(0, 10):
-    valor = str(input('Digite um número de 0 a 9: '))
-    lista += valor
-print(lista)
+primeiro = int(input('Primeiro termo: '))
+razao = int(input('Razão: '))
+decimo = primeiro + (10 - 1) * razao
+for c in range(primeiro, decimo + razao, razao):
+    print('{} '.format(c), end='-> ')
+print('ACABOU')
