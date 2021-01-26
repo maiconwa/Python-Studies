7bde5fb0c5d804789e9aef328d6515df3b2b02a4
Maicon William <73193175+maiconwa@users.noreply.github.com>
2021-01-26 09:40:11 -0300

    Update ex053

--- Exercícios/ex053.py
+++ Exercícios/ex053.py
@@ -1,2 +1,7 @@
 """Desafio 053:
-Crie um programa que leia uma frase qualquer e diga se ela é um políndromo, desconsiderando os espaços."""
+Crie um programa que leia uma frase qualquer e diga se ela é um palíndromo, desconsiderando os espaços.
+Ex: APOS A SOPA
+Ex: A SACADA DA CASA
+Ex: A TORRE DA DERROTA
+Ex: O LOBO AMA O BOLO
+Ex: ANOTARAM A DATA DA MARATONA"""
