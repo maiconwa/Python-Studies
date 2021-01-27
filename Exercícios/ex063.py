326341550060162de3d35499d13e4811de678dcd
Maicon William <73193175+maiconwa@users.noreply.github.com>
2021-01-27 10:11:03 -0300

    Create ex063.py

---
+++ Exercícios/ex063.py
@@ -0,0 +1,5 @@
+"""Desafio 063:
+Escreva um programa que leia um número n inteiro qualquer e mostre na tela os n primeiros elementos de uma
+SEQUÊNCIA DE FIBONACCI.
+
+Ex: 0 -> 1 -> 2 -> 3 -> 5 -> 8"""
