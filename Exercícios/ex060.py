138bc827f7307028a3177df3680bbaaea1e1ee28
Maicon William <73193175+maiconwa@users.noreply.github.com>
2021-01-27 13:19:19 -0300

    Update ex060.py

--- Exercícios/ex060.py
+++ Exercícios/ex060.py
@@ -4,3 +4,9 @@ Faça um programa que leia um número qualquer e mostre o seu fatorial.
 Exemplo: 5; = 5x4x3x2x1 = 120
 
 com enquanto e for"""
+
+num1 = int(input('Digite um número a ser fatorado: '))
+num2 = 0
+while num2 > 1:
+    print(num2, 'X')
+    num2 = num1 - 1
