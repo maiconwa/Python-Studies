a79ebc4522d12cb06010cf8e4a40352e1191fc83
Maicon William <73193175+maiconwa@users.noreply.github.com>
2021-01-27 13:54:23 -0300

    Update ex060.py

--- Exercícios/ex060.py
+++ Exercícios/ex060.py
@@ -5,8 +5,12 @@ Exemplo: 5; = 5x4x3x2x1 = 120
 
 com enquanto e for"""
 
-num1 = int(input('Digite um número a ser fatorado: '))
 num2 = 0
-while num2 > 1:
-    print(num2, 'X')
+num1 = int(input('Digite um número a ser fatorado: '))
+acumulador = 0
+while num1 > 1:
+    print(num1, 'X ', num2)
+    if num1 >= 1:
+        acumulador = num1 * num1
     num2 = num1 - 1
+print(acumulador)
