3b294b45738a2a982d8ca3fd4fe94e2061c2ad45
Maicon William <73193175+maiconwa@users.noreply.github.com>
2021-01-27 10:15:29 -0300

    Create ex64.py

---
+++ Exercícios/ex64.py
@@ -0,0 +1,5 @@
+"""Desafio 064:
+Crie um programa que leia vários números inteiros pelo teclado. O programa só vai parar quando o usuário digitar
+qqq, que é a condição de parada. No final mostre quantos números foram digitados e qual foi a soma entre eles.
+(Desconsiderando o flag)
+flag = condição de parada = qqq """
