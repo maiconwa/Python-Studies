cc2e6eb9dedb23fb04e48534e6d575126f1acdc3
Maicon William <73193175+maiconwa@users.noreply.github.com>
2021-01-27 20:52:24 -0300

    Update ex061.py

--- Exercícios/ex061.py
+++ Exercícios/ex061.py
@@ -8,3 +8,7 @@ decimo = primeiro + (10 - 1) * razao
 for c in range(primeiro, decimo + razao, razao):
     print('{} '.format(c), end='-> ')
 print('ACABOU')
+
+primeiro = int(input('Primeiro termo: '))
+razao = int(input('Razao: '))
+decimo = primeiro + (10 - 1) * razao
