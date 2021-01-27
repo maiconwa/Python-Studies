8da5942afe8e7547904ed3de298a1cd174c72224
Maicon William <73193175+maiconwa@users.noreply.github.com>
2021-01-27 09:11:33 -0300

    Update ex056.py

--- Exercícios/ex056.py
+++ Exercícios/ex056.py
@@ -4,6 +4,8 @@ Desenvolva um programa que leia o nome, idade e sexo de 4 pessoas. No final do p
 - Qual é o homem mais velho.
 - Quantas mulheres têm menos de 20 anos."""
 
+""" Minha forma de fazer: 
+
 from datetime import date
 ano = date.today().year
 atual = 0
@@ -30,3 +32,31 @@ for c in range(1, 5):
 print('A média de idade de todo o grupo é {}.'.format(media / grupo))
 print('O homem mais velho é o {}ª a sua idade é {}.'.format(homemmaisvelho, homemvelho))
 print('No total {} mulheres tem menos de 20 anos.'.format(mulheres))
+
+"""
+
+""" Correção: Desafio 056 """
+
+somaidade = 0
+mediaidade = 0
+maioridadehomem = 0
+nomevelho = ''
+totmulher20 = 0
+for p in range(1 ,5):
+    print('----- {}ª PESSOA ------'.format(p))
+    nome = str(input('Nome: ')).strip()
+    idade = int(input('Idade: '))
+    sexo = str(input('Sexo [M/F]: ')).strip()
+    somaidade += idade
+    if p == 1 and sexo in 'Mm':
+        maioridadehomem = idade
+        nomevelho = nome
+    if sexo in 'Mm' and idade > maioridadehomem:
+        maioridadehomem = idade
+        nomevelho = nome
+    if sexo in 'Ff' and idade < 20:
+        totmulher20 += 1
+mediaidade = somaidade / 4
+print('A média de idade do grupo é de {} anos.'.format(mediaidade))
+print('O homem mais velho tem {} anos e se chama {}.'.format(maioridadehomem, nomevelho))
+print('Ao todo são {} mulheres com menos de 20 anos.'.format(totmulher20))
