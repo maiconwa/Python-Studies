a6e8b464278d4c8193781b021924d930034ead5b
Maicon William <73193175+maiconwa@users.noreply.github.com>
2021-01-27 19:42:42 -0300

    Update ex061.py

--- Exercícios/ex061.py
+++ Exercícios/ex061.py
@@ -1,3 +1,10 @@
 """Desafio 061:
 Refaça o desafio 051, lendo o primeiro termo e a razão da PA, mostrando os 10 primeiros termos da progressão
 usando a estrutura while."""
+
+primeiro = int(input('Primeiro termo: '))
+razao = int(input('Razão: '))
+decimo = primeiro + (10 - 1) * razao
+for c in range(primeiro, decimo + razao, razao):
+    print('{} '.format(c), end='-> ')
+print('ACABOU')
