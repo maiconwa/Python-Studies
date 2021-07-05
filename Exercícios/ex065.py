ea44b873912aa9dd7f58b759dcc793f199a923d8
Maicon William <73193175+maiconwa@users.noreply.github.com>
2021-07-05 18:43:00 -0300

    Update ex065.py

--- Exercícios/ex065.py
+++ Exercícios/ex065.py
@@ -2,3 +2,7 @@
 Crie um programa que leia vários números inteiros pelo teclado. No final da execução, mostre a média entre todos
 os valores e qual foi o maior e o menor valor lidos. O programa deve perguntar ao usuário se ele quer ou não
 continuar a digitar valores. """
+
+
+n = int(input("Digite um valor: "))
+print(n)
